74c2ea97870aa7bc0d1538a7ac95c2961e1efdb1
Nathan Quintão <nathanquintaounip@gmail.com>
2025-09-02 21:29:58 -0300

    Update aula02.java

--- aula02.java
+++ aula02.java
@@ -5,34 +5,53 @@
 package aula01;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  *
  * @author ALUNO
  */
 public class aula02 extends JFrame {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Exemplo swing");
-        frame.setSize(400, 200);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
+    
+    public aula02(){
+        setTitle("Exemplo swing");
+        setSize(600, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        mainPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        
         JLabel label = new JLabel("Digite seu nome:");
-        label.setBounds(20, 20, 150, 25);
+        label.setBounds(20, 20, 15, 25);
+        
         
         JTextField campo = new JTextField();
-        campo.setBounds(150, 20, 200, 25);
+        campo.setMaximumSize(new Dimension(80,30));
         
         JButton botao = new JButton("Enviar");
-        botao.setBounds(150, 60, 100, 30);
+        botao.setMaximumSize(new Dimension(100,50));
+        botao.setAlignmentX(Component.CENTER_ALIGNMENT);
+    
         botao.addActionListener(e -> {
             campo.setText("");
             JOptionPane.showMessageDialog(null, "Enviado com sucesso!");
         });
         
-        frame.add(label);
-        frame.add(campo);
-        frame.add(botao);
         
-        frame.setVisible(true);
+        mainPanel.add(label);
+        mainPanel.add(campo);
+        mainPanel.add(botao);
+        
+        add(mainPanel);
+        
+        setVisible(true);
+    }
+     public static void main(String[] args) {
+    
+        new aula02();
     }
 }
+
